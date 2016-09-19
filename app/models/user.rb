7166f0803c2c98a7666ba3da2b71a0b839e2c2ff
tangjiahong <fruitcake0525@gmail.com>
2016-09-19 23:54:13 +0800

    model user through: :user_events

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,5 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   has_many :user_events
-  has_many :events
+  has_many :events, through: :user_events
 end
