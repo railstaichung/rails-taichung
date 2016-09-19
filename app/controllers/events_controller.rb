aa6f57e64f2fe01e662b86b0131140b6aee70b19
tangjiahong <fruitcake0525@gmail.com>
2016-09-20 00:12:04 +0800

    controller events CRUD

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,2 +1,37 @@
 class EventsController < ApplicationController
+  before_action :authenticate_user!, only: [:edit, :update, :destroy]
+
+  def index
+    @events = Event.all.order("created_at DESC")
+  end
+
+  def show
+    @event = Event.find(params[:id])
+  end
+
+  def edit
+    @event = current_user.events.find(params[:id])
+  end
+
+  def update
+    @event = current_user.events.find(params[:id])
+
+    if @event.update(event_params)
+      redirect_to event_path(@event)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @event = current_user.events.find(params[:id])
+    @event.destroy
+    redirect_to events_path
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:topic, :start_time, :end_time, :location, :owner, :content)
+  end
 end
