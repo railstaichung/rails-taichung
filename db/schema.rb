e5aad7937600a01197f28277f2d01120df0fe8b1
tangjiahong <fruitcake0525@gmail.com>
2016-09-19 23:43:04 +0800

    rails generate simple_form:install --bootstrap

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160912093945) do
+ActiveRecord::Schema.define(version: 20160919153653) do
+
+  create_table "events", force: :cascade do |t|
+    t.string   "topic"
+    t.datetime "start_time"
+    t.datetime "end_time"
+    t.string   "location"
+    t.string   "owner"
+    t.text     "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
