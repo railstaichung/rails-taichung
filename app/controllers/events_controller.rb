b60f09ab9db11fb59aa49521f956006e8ef02069
tangjiahong <fruitcake0525@gmail.com>
2016-09-20 20:29:13 +0800

    view app/views/events/new&edit fix

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,5 @@
 class EventsController < ApplicationController
-  before_action :authenticate_user!, only: [:edit, :update, :destroy]
+  #before_action :authenticate_user!, only: [:new, :edit, :update, :destroy]
 
   def index
     @events = Event.all.order("created_at DESC")
