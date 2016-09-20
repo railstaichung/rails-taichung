65ea5e8951d1e8b8a449b06a4999ca5c6280c0a0
tangjiahong <fruitcake0525@gmail.com>
2016-09-20 21:37:40 +0800

    view app/views/events/show 完成

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,5 @@
 class EventsController < ApplicationController
-  #before_action :authenticate_user!, only: [:new, :edit, :update, :destroy]
+#  before_action :authenticate_user!, only: [:edit, :update, :destroy]
 
   def index
     @events = Event.all.order("created_at DESC")
@@ -9,16 +9,27 @@ class EventsController < ApplicationController
     @event = Event.new
   end
 
+  def create
+    @event = Event.new(event_params)
+
+    if @event.save
+    # @event.owner = @current_user.user_info.name
+      redirect_to event_path(@event)
+    else
+      render :new
+    end
+  end
+
   def show
     @event = Event.find(params[:id])
   end
 
   def edit
-    @event = current_user.events.find(params[:id])
+    @event = Event.find(params[:id])
   end
 
   def update
-    @event = current_user.events.find(params[:id])
+    @event = Event.find(params[:id])
 
     if @event.update(event_params)
       redirect_to event_path(@event)
@@ -28,7 +39,7 @@ class EventsController < ApplicationController
   end
 
   def destroy
-    @event = current_user.events.find(params[:id])
+    @event = Event.find(params[:id])
     @event.destroy
     redirect_to events_path
   end
