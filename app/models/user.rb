953b8a307a2e6c6ae1d190b4b767735db5e0d860
Kenny Chiu <kennychiu@jourdeness.com.tw>
2016-09-28 09:09:05 +0800

    完成密碼拾回機制

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,13 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :confirmable
+  devise :database_authenticatable,
+         :registerable,
+         :recoverable,
+         :rememberable,
+         :trackable,
+         :validatable,
+         :confirmable
 
   has_many :user_events
   has_many :events, through: :user_events
