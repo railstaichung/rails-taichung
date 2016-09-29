e005ea78225cc09538bb89e14ac11a9948b6310c
leo424y <leo424y@gmail.com>
2016-09-29 20:15:06 +0800

    9/29 update from master

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,13 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  devise :database_authenticatable,
+         :registerable,
+         :recoverable,
+         :rememberable,
+         :trackable,
+         :validatable,
+         :confirmable
 
   has_many :user_events
   has_many :participated_events, through: :user_events, source: :event
