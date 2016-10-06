9e29968aa26f1f63c374800de68827b023ab70f5
leo424y <leo424y@gmail.com>
2016-10-06 16:02:39 +0800

    add gravtastic as user photo

--- Gemfile
+++ Gemfile
@@ -35,6 +35,7 @@ gem 'holder_rails'
 gem 'haml-rails'
 gem 'carrierwave'
 gem 'mini_magick'
+gem 'gravtastic'
 
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
   class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  include Gravtastic
+  gravtastic
+
   devise :database_authenticatable,
          :registerable,
          :recoverable,
