10fc1ec23a47012a60cd470f9d1b805872a5856d
hungmi <hungmihungmi@gmail.com>
2016-10-07 21:25:27 +0800

    寫測試用 seed 活動 #14 在 seed 檔加入可創建五個事件 此外，在 events#index, show 使用 try 避免 event_photo nil 的錯誤

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,3 +16,16 @@ create_profiles = for i in 1..5 do
                    Profile.create([content: "連結 #{k} ", user_id: "#{i}"])
                  end
                 end
+
+user_ids = User.all.pluck(:id)
+
+for e in 1..5 do
+  Event.create(
+    topic: "如果我們是學生",
+    start_time: 7.days.ago,
+    end_time: 7.days.since,
+    location: ["台中市", "台北市"][rand(0..1)],
+    content: "他的聲音變弱，我爸在也在醫科教書，生活花費比較便宜，很棒同時也是很悲傷的事情是，我在2011年春天被調去上海前曾在這裡住過一年，突然間，如果我們是學生，我們在亞洲教育中被教導的許多重大選擇，看看左側的夜景和右邊的老建築和銀行。請粉絲們持續留意各項報導，怎麼就是有人那麼再意臉書上寫什麼...以為幫別人求婚，林說，嘖嘖~，然而被騷擾的不悅感與日俱增，最愛女人露哪裡，漁業署今發出新聞稿表示，林義雄將禁食。",
+    user_id: user_ids[rand(0 .. user_ids.size - 1)]
+  )
+end
