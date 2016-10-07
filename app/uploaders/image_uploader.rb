61c095594e705473fcf2296d15d6acc2b1a1774a
leo424y <leo424y@gmail.com>
2016-10-07 13:35:25 +0800

    thumb 補回
    歹勢誤刪

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -24,6 +24,9 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   process resize_to_fit: [1280,360]
 
+  version :thumb do
+    process resize_to_fit: [300,300]
+  end
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
