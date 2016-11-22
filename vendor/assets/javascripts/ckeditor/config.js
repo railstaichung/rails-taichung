0a319d2815515b1bd2814ddb1b46f192b0c8d0db
邱兆弘 <koei1025@gmail.com>
2016-11-22 13:50:13 +0800

    處理rails console無法執行問題

--- vendor/assets/javascripts/ckeditor/config.js
+++ vendor/assets/javascripts/ckeditor/config.js
@@ -1,12 +1,11 @@
 CKEDITOR.editorConfig = function (config) {
-    config.extraPlugins = 'imgur';
     // Get your client-id from https://api.imgur.com/oauth2/addclient
     config.imgurClientID = '4005c71208be9df';
     config.toolbar_mini = [
       ['Styles', 'Format', 'Font', 'FontSize'],
       ["Bold",  "Italic",  "Underline",  "Strike",  "-",  "Subscript",  "Superscript"],
       ['Image', 'Table', 'HorizontalRule', 'Smiley', 'SpecialChar'],
-      ['pbckcode']
+      ['pbckcode','Imgur']
     ];
     config.toolbar_cust = [['Undo', 'Redo', '-', 'SelectAll', 'RemoveFormat'],
                            ['Styles', 'Format', 'Font', 'FontSize'],
@@ -16,8 +15,9 @@ CKEDITOR.editorConfig = function (config) {
                            ['NumberedList', 'BulletedList', '-', 'Outdent', 'Indent', 'Blockquote', 'pbckcode'],
                            ['JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock'],
                            ['Link', 'Unlink', 'Anchor'],
-                           ['Image', 'Table', 'HorizontalRule', 'Smiley', 'SpecialChar', 'PageBreak']];
+                           ['Imgur', 'Table', 'HorizontalRule', 'Smiley', 'SpecialChar', 'PageBreak']];
     config.toolbar = "simple";
     config.extraPlugins = 'pbckcode';
+    config.extraPlugins = 'imgur';
 
 }
