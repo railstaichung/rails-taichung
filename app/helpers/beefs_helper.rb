1f141d31e18cf402c3e5296ac7b6b4c57bb918c4
邱兆弘 <koei1025@gmail.com>
2016-11-23 17:36:24 +0800

    Beef多語系與細部調整

--- app/helpers/beefs_helper.rb
+++ app/helpers/beefs_helper.rb
@@ -5,16 +5,16 @@ module BeefsHelper
     time_gap = Time.now - created_at
     if time_gap < 1.day.second then
       diff = time_gap / 1.hour.second
-      time_result = "#{diff.floor} 小時前發表"
+      time_result = "#{diff.floor} #{I18n.t('resource.hours_ago_submit')},"
     elsif time_gap >= 1.day.second && time_gap < 1.month.second then
       diff = time_gap / 1.day.second
-      time_result = "#{diff.floor} 天前發表"
+      time_result = "#{diff.floor} #{I18n.t('resource.days_ago_submit')},"
     elsif time_gap >= 1.month.second && time_gap < 1.year.second then
       diff = time_gap / 1.month.second
-      time_result = "#{diff.floor} 月前發表"
+      time_result = "#{diff.floor} #{I18n.t('resource.months_ago_submit')},"
     elsif time_gap >= 1.year.second then
       diff = time_gap / 1.year.second
-      time_result = "#{diff.floor} 年前發表"
+      time_result = "#{diff.floor} #{I18n.t('resource.years_ago_submit')}"
     end
     return time_result
   end
